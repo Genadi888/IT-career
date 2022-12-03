c357be5eabb5efe4a1bd86e347dab4787afa5555
Genadi888 <genadi.fidanov@abv.bg>
2022-12-03 18:41:33 +0200

    Add project files.

---
+++ Повторения - изпити/2. Деление без остатък/Program.cs
@@ -0,0 +1,34 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Number count: ");
+        double numberCount = double.Parse(Console.ReadLine());
+        double multiplesOf2 = 0;
+        double multiplesOf3 = 0;
+        double multiplesOf4 = 0;
+
+        for (int i = 0; i < numberCount; i++)
+        {
+            Console.Write("Number: ");
+            double n = double.Parse(Console.ReadLine());
+
+            if (n % 2 == 0)
+            {
+                multiplesOf2++;
+            }
+            if (n % 3 == 0)
+            {
+                multiplesOf3++;
+            }
+            if (n % 4 == 0)
+            {
+                multiplesOf4++;
+            }
+        }
+
+        Console.WriteLine("{0:f2}%", (multiplesOf2 / numberCount) * 100);
+        Console.WriteLine("{0:f2}%", (multiplesOf3 / numberCount) * 100);
+        Console.WriteLine("{0:f2}%", (multiplesOf4 / numberCount) * 100);
+    }
+}
---
+++ Повторения - изпити/3. Чертане на крепост/Program.cs
@@ -0,0 +1,61 @@
+﻿class Program
+{
+    static private int dashesBetweenStarsCount = 0;
+    static int leftDashesCount = 0;
+    static int rightDashesCount = 0;
+    static int drawingWidth = 0;
+
+    static void Main(string[] args)
+    {
+        Console.Write("n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        leftDashesCount = n * 3;
+        drawingWidth = n * 5;
+        rightDashesCount = drawingWidth - leftDashesCount - 2;
+
+        PrintTopPart(n);
+        PrintCenterPart(n);
+        PrintBottomPart(n);
+    }
+
+    static void PrintTopPart(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine(new string('-', n * 3) + '*' + new string('-', dashesBetweenStarsCount) + '*'
+                + new string('-', rightDashesCount));
+
+            if (i != n - 1)
+            {
+                dashesBetweenStarsCount++;
+                rightDashesCount--;
+            }
+        }
+    }
+
+    static void PrintCenterPart(int n)
+    {
+        for (int i = 0; i < n / 2; i++)
+        {
+            Console.WriteLine(new string('*', n * 3) + '*' + new string('-', dashesBetweenStarsCount) + '*'
+                + new string('-', drawingWidth - leftDashesCount - (dashesBetweenStarsCount + 2)));
+        }
+    }
+
+    static void PrintBottomPart(int n)
+    {
+        for (int i = 0; i < n / 2; i++)
+        {
+            if (i == n / 2 - 1)
+            {
+                Console.WriteLine(new string('-', leftDashesCount--) + "*" + new string('*', dashesBetweenStarsCount++) + "**" + new string('-', rightDashesCount--));
+            }
+            else if (n > 3)
+            {
+                Console.WriteLine(new string('-', leftDashesCount--) + "*" + new string('-', dashesBetweenStarsCount) + "*" + new string('-', rightDashesCount--));
+                dashesBetweenStarsCount += 2;
+            }
+        }
+    }
+}
