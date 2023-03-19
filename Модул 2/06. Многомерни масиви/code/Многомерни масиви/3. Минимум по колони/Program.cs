680b62904e4f5abaf6da256c9eef7db567b1d310
Genadi888 <genadi.fidanov@abv.bg>
2023-03-19 11:28:51 +0200

    solved "Lottery ticket"

---
+++ Модул 2/06. Многомерни масиви/code/Многомерни масиви/3. Минимум по колони/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace _3._Минимум_по_колони
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Rows: ");
+            int rows = int.Parse(Console.ReadLine());
+            rows++;
+            Console.Write("Cols: ");
+            int cols = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    if (row == rows - 1) // set min value for column
+                    {
+                        int smallestNum = int.MaxValue;
+                        for (int rowIndex = 0; rowIndex < rows - 1; rowIndex++) // find min value
+                        {
+                            if (matrix[rowIndex, col] < smallestNum)
+                            {
+                                smallestNum = matrix[rowIndex, col];
+                            }
+                        }
+                        matrix[row, col] = smallestNum;
+                        continue;
+                    }
+
+                    Console.Write("Enter num for [{0}][{1}]: ", row, col);
+                    matrix[row, col] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    Console.Write("{0, 10}", matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Модул 2/06. Многомерни масиви/code/Многомерни масиви/4.  Лотариен Билет/Program.cs
@@ -0,0 +1,110 @@
+﻿namespace _4.__Лотариен_Билет
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter rows and cols: ");
+            string input = Console.ReadLine();
+            int rows = int.Parse(input.Split(' ')[0]);
+            int cols = int.Parse(input.Split(' ')[1]);
+
+            int[,] ticket = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++) // entering numbers
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    Console.Write("Enter num for [{0}][{1}]: ", row, col);
+                    ticket[row, col] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            if (CheckIfTicketIsWinning(ticket))
+            {
+                Console.WriteLine("YES");
+                Console.WriteLine("The amount of money won is: {0:F2}", CalculatePrize(ticket));
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+
+        static bool CheckIfTicketIsWinning(int[,] ticket)
+        {
+            int mainDiagonalSum = 0;
+            int secondaryDiagonalSum = 0;
+            int sumAboveMainDiag = 0;
+            int sumBelowMainDiag = 0;
+
+            for (int row = 0; row < ticket.GetLength(0); row++) // entering numbers
+            {
+                for (int col = 0; col < ticket.GetLength(1); col++)
+                {
+                    int curNum = ticket[row, col];
+
+                    if (row == col)
+                    {
+                        mainDiagonalSum += curNum;
+                    }
+
+                    if (row < col)
+                    {
+                        sumAboveMainDiag += curNum;
+                    }
+                    
+                    if (row > col)
+                    {
+                        sumBelowMainDiag += curNum;
+                    }
+
+                    if (row + col == ticket.GetLength(0) - 1)
+                    {
+                        secondaryDiagonalSum += curNum;
+                    }
+                }
+            }
+
+            return mainDiagonalSum == secondaryDiagonalSum && sumAboveMainDiag % 2 == 0 && sumBelowMainDiag % 2 != 0;
+        }
+
+        static double CalculatePrize(int[,] ticket)
+        {
+            double sumBelowMainDiag = 0d;
+            double evenNumsMainDiagonalSum = 0d;
+            double evenNumsBorderRowsSum = 0d;
+            double oddNumsBorderColsSum = 0d;
+
+            for (int row = 0; row < ticket.GetLength(0); row++) // entering numbers
+            {
+                for (int col = 0; col < ticket.GetLength(1); col++)
+                {
+                    int curNum = ticket[row, col];
+
+                    if (row > col)
+                    {
+                        sumBelowMainDiag += curNum;
+                    }
+
+                    if (row == col && curNum % 2 == 0)
+                    {
+                        evenNumsMainDiagonalSum += curNum;
+                    }
+
+                    if ((row == 0 || row == ticket.GetLength(0) - 1) && curNum % 2 == 0)
+                    {
+                        evenNumsBorderRowsSum += curNum;
+                    }
+                    
+                    if ((col == 0 || col == ticket.GetLength(1) - 1) && curNum % 2 != 0)
+                    {
+                        oddNumsBorderColsSum += curNum;
+                    }
+                }
+            }
+
+            return (sumBelowMainDiag + evenNumsMainDiagonalSum + evenNumsBorderRowsSum + oddNumsBorderColsSum) / 4;
+        }
+    }
+}
