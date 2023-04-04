82ea71073c5cbf1090c3a6ef2503b8cabc226c10
Genadi888 <genadi.fidanov@abv.bg>
2023-04-04 21:59:08 +0300

    solved "Anonymous Downsite"

---
+++ Модул 2/08. Практически изпит/code/Изпитни задачи/Anonymous Downsite/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Anonymous_Downsite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number of sites: ");
+            int sitesCount = int.Parse(Console.ReadLine());
+            Console.Write("Enter security key: ");
+            int key = int.Parse(Console.ReadLine());
+
+            decimal totalLoss = 0m;
+            List<string> sitesNames = new List<string>();
+
+            for (int i = 0; i < sitesCount; i++)
+            {
+                Console.Write("Enter site and its info: ");
+                string[] siteInfo = Console.ReadLine().Split(' ');
+                string siteName = siteInfo[0];
+                decimal siteVisits = decimal.Parse(siteInfo[1]);
+                decimal siteCommercialPricePerVisit = decimal.Parse(siteInfo[2]);
+
+                totalLoss += siteVisits * siteCommercialPricePerVisit;
+                sitesNames.Add(siteName);
+            }
+            
+            for (int i = 0; i < sitesCount; i++)
+            {
+                Console.WriteLine(sitesNames[i]);
+            }
+
+            Console.WriteLine("Total Loss: {0:F20}", totalLoss);
+            Console.WriteLine("Security Token: {0}", Math.Pow(key, sitesCount));
+
+        }
+    }
+}
