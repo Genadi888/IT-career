472a58f77212143c78c5334487946f62d887a814
Genadi888 <genadi.fidanov@abv.bg>
2023-07-13 16:58:57 +0300

    solved "5. Генериране на комбинации"

---
+++ Модул 4/code/Рекурсия/3. Рекурсивно чертаене/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _3._Рекурсивно_чертаене
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Enter n: ");
+			int n = int.Parse(Console.ReadLine());
+			PrintFigure(n);
+		}
+
+		private static void PrintFigure(int n)
+		{
+			if (n == 0)
+			{
+				return;
+			}
+
+			Console.WriteLine(new string('*', n));
+			PrintFigure(n - 1);
+			Console.WriteLine(new string('#', n));
+		}
+	}
+}
---
+++ Модул 4/code/Рекурсия/5. Генериране на комбинации/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _5._Генериране_на_комбинации
+{
+	internal class Program
+	{
+		private static List<int> inputList;
+		private static int groupLength;
+
+		static void Main(string[] args)
+		{
+			Console.Write("Enter list: ");
+			inputList = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+			Console.Write("Enter k: ");
+			groupLength = int.Parse(Console.ReadLine());
+			PrintCombinations(0, groupLength - 1);
+		}
+
+		private static void PrintCombinations(int baseIndex, int curIndex)
+		{
+			List<int> printList = new List<int>();
+
+			printList.AddRange(inputList.GetRange(baseIndex, groupLength - 1));
+			printList.Add(inputList[curIndex]);
+
+			Console.WriteLine(String.Join(' ', printList));
+
+			//int nextCurIndex = baseIndex + groupLength;
+
+			if (baseIndex == inputList.Count - groupLength) //bottom of recursion
+			{
+				return;
+			}
+
+			if (curIndex + 1 == inputList.Count)
+			{
+				PrintCombinations(baseIndex + 1, baseIndex + groupLength);
+			} 
+			else
+			{
+				PrintCombinations(baseIndex, curIndex + 1);
+			}
+		}
+	}
+}
